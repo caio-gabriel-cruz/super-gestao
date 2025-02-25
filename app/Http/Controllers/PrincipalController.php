053f1fab57a2c5d2ef5a9e85b7c55aff9e5d8946
caio-gabriel-cruz <killua37pt@gmail.com>
2025-02-25 20:12:20 -0300

    ultima pae

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function principal(){
-        echo 'faz muito tempo que não gozo';
+        return view("site.principal");
     }
 }
